374d7ee7bb4cdef1e17d17cf23fb6512512d391c
Daniel Khromov <danielkhromov@gmail.com>
2022-09-24 23:21:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import streamlit as st
 
 st.title('Расчет цены ипотеки')
 
-start = st.number_input('Начальная цену квартиры в млн (например, 8)', min_value=0)
-sell = st.number_input('Цена при продаже в млн (например, 10)', min_value=0)
-delta_tax = st.number_input('Налог при продаже (%)', value=13)
+buy = st.number_input('Первоначальная цена квартиры (полностью, например, 8200000)', min_value=0, format='%i')
+sell = st.number_input('Цена при продаже на руки продавцу (полностью, например, 10500000)', min_value=0)
+tax = st.number_input('Подоходный налог при завышении (%)', value=13)
 fee = st.number_input('Первоначальный взнос (%)', min_value=0, value=20)
 
-start*=10**6
-sell*=10**6
-delta_tax/=100
-fee/=100
+tax /= 100
+fee /= 100
 
 st.title('Результат:')
-res = (sell*(delta_tax-1))/(fee+delta_tax+-1)
-st.title('{:,}'.format(round(res, 2)).replace(',', ' ')+' руб')
-
+res = (sell - buy * tax) / (1 - tax - fee)
+st.title('{:,}'.format(round(res, 2)).replace(',', ' ') + ' руб')
