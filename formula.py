93d6071fb8b2be29266dfeb0defe56873a2b4517
Daniel Khromov <danielkhromov@gmail.com>
2022-10-17 11:00:03 +0300

    update for no fee

--- formula.py
+++ formula.py
@@ -1,9 +1,9 @@
-def formula_with_percent(sell, buy, tax, fee):
+def formula_without_entry_fee(sell, buy, tax, fee):
     fee /= 100
     tax /= 100
     return (sell - buy * tax) / (1 - tax - fee)
 
 
-def formula_without_percent(sell, buy, tax, fee):
-    tax /= 100
-    return (sell + fee - buy * tax) / (1 - tax)
+def formula_with_entry_fee(Arms, Ready, Initial):
+    # A - R = Final - (Final - I)*0.13
+    return (Arms - Ready - 0.13*Initial) / (1-0.13)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from formula import *
+import math
 
 hide_menu_style = """
         <style>
@@ -9,20 +10,36 @@ hide_menu_style = """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.title('Расчет цены ипотеки')
 res = 0
-buy = st.number_input('Первоначальная цена квартиры (руб) (полностью, например, 8200000)', min_value=0, format='%i')
-sell = st.number_input('Цена при продаже на руки продавцу (руб) (полностью, например, 10500000)', min_value=0)
-tax = st.number_input('Подоходный налог при завышении (%)', value=13)
-form = st.selectbox('Первоначальный взнос (проценты/рубли)', options=['Проценты', 'Рубли'])
-if form == 'Проценты':
+tax = 13  # подоходный налог
+form = st.selectbox('Первоначальный взнос есть?', options=['Не выбрано', 'Да', 'Нет'], )
+if form == 'Нет':
+    buy = st.number_input('Первоначальная цена квартиры в руб (полностью, например, 8200000)', min_value=0)
+    sell = st.number_input('Цена при продаже на руки продавцу в руб (полностью, например, 10500000)', min_value=0)
     fee = st.number_input('Первоначальный взнос (%)', min_value=0, value=20)
-    res = formula_with_percent(sell, buy, tax, fee)
-else:
-    fee = st.number_input('Первоначальный взнос (полностью, в рублях)', min_value=0, value=0)
-    res = formula_without_percent(sell, buy, tax, fee)
+    res = formula_without_entry_fee(sell, buy, tax, fee)
 
+    st.subheader('Стоимость квартиры в ипотеку с завышением:')
+    if res == 0:
+        st.text('Сначала введите все данные')
+    else:
+        st.title('{:,}'.format(round(res, 2)).replace(',', ' ') + ' руб')
 
-st.title('Результат:')
-if res == 0:
-    st.text('Сначала введите все данные')
-else:
-    st.title('{:,}'.format(round(res, 2)).replace(',', ' ') + ' руб')
+if form == 'Да':
+    in_arms = st.number_input('Стоимость на руки продавцу в руб (полностью, например, 10500000)', min_value=0)
+    dkp_seller = st.number_input('Стоимость в ДКП в руб (полностью)', min_value=0)
+    initial = st.number_input('Первоначальная цена квартиры в руб (полностью, например, 8200000)', min_value=0,
+                              format='%i')
+    bank = st.number_input('Банковский процент (%)', min_value=15)
+    res = round(math.ceil(formula_with_entry_fee(in_arms, (in_arms - dkp_seller) * 0.13, initial)))
+    if res == 0:
+        st.text('Сначала введите все данные')
+    else:
+        st.subheader('Стоимость квартиры в ипотеку с занижением:')
+        st.subheader('{:,}'.format(res).replace(',', ' ') + ' руб')
+        st.subheader('Первоначальный взнос: ')
+        st.subheader('{:,}'.format(res * bank / 100).replace(',', ' ') + ' руб')
+        st.subheader('Сумма налога: ')
+        st.subheader('{:,}'.format((res - initial) * 0.13).replace(',', ' ') + ' руб ')
+        st.text('Из них компенсирует продавец: ' + '{:,}'.format((dkp_seller - initial) * 0.13))
+        st.subheader('Покупатель компенсирует: ')
+        st.subheader('{:,}'.format((res - initial) * 0.13 - (dkp_seller - initial) * 0.13))
